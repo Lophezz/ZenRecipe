808d4880b13d5f64e0878e9b474e3fc261e61528
Matheus Lopes <107969085+Lophezz@users.noreply.github.com>
2025-10-31 18:55:03 -0300

    Add index.html to Tailwind CSS config content

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./home.html"  // <--- Garanta que esta linha exista!
+    "./index.html"  // <--- Garanta que esta linha exista!
   ],
   theme: {
     extend: {},
