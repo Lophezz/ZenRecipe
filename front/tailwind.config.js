9859a0db919f545cf0c53a8673e3d49833a14015
Lophezz <lopes.matheus334@gmail.com>
2025-10-31 18:45:07 -0300

    Corrige build do Tailwind v3 e adiciona gitignore

---
+++ front/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./home.html"  // <--- Garanta que esta linha exista!
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
